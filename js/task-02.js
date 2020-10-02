254631e4ac30f884fa0b9d64252f9bd4515d710e
skrypkasv <skrypkasv@gmail.com>
2020-10-02 19:56:00 +0300

    Adds task_02, 03

--- js/task-02.js
+++ js/task-02.js
@@ -1,1 +1,25 @@
-console.log('test');
+// В HTML есть пустой список ul#ingredients.
+// В JS есть массив строк.
+
+// Напиши скрипт, который для каждого элемента
+// массива ingredients создаст отдельный li,
+// после чего вставит все li за одну операцию в
+// список ul#ingredients. Для создания DOM - узлов
+// используй document.createElement().
+
+const ingredients = [
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
+];
+
+const ulRef = document.querySelector('ul#ingredients');
+
+ingredients.map(item => {
+  const liRef = document.createElement('li');
+  liRef.textContent = item;
+  ulRef.append(liRef);
+});
---
+++ js/task-03.js
@@ -0,0 +1,47 @@
+// Напиши скрипт для создания галлереи
+// изображений по массиву данных.
+
+// В HTML есть список ul#gallery.
+
+// Используй массив объектов images для создания
+// тегов img вложенных в li. Для создания
+// разметки используй шаблонные строки и
+// insertAdjacentHTML().
+
+// ▶ Все элементы галереи должны добавляться в
+// DOM за одну операцию вставки.
+
+// ▶ Добавь минимальное оформление галереи
+// флексбоксами или гридами через css - классы.
+
+const images = [
+  {
+    url:
+      'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+];
+
+const ulRef = document.querySelector('ul#gallery');
+console.log('ulRef', ulRef);
+
+images.map(({ url, alt }) => {
+  ulRef.insertAdjacentHTML(
+    'beforeend',
+    `<li><img src="${url}" alt="${alt}" width="320"></li>`,
+  );
+});
+
+ulRef.style.display = 'flex';
+ulRef.style.listStyle = 'none';
+ulRef.style.justifyContent = 'space-between';
